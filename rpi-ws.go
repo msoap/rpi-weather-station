bac8331aabe169d0921b3651cddf5aaf8672200d
Serhii Mudryk <sergey.mudrik@gmail.com>
2025-11-23 00:44:24 +0200

    Added text shifting to avoid OLED burn-in

--- rpi-ws.go
+++ rpi-ws.go
@@ -33,7 +33,19 @@ func main() {
 		log.Fatalf("Failed to initialize BME280: %v", err)
 	}
 
+	shiftX, shiftY, i := 0, 0, 0 // for shift printed text to avoid burn-in oled screen
 	for range time.Tick(time.Second * 10) {
+		switch i % 4 {
+		case 0:
+			shiftX, shiftY = 0, 0
+		case 1:
+			shiftX, shiftY = 0, 1
+		case 2:
+			shiftX, shiftY = 1, 0
+		case 3:
+			shiftX, shiftY = 0, 1
+		}
+
 		temp, pres, err := bme.Read()
 		if err != nil {
 			log.Fatalf("Failed to read BME280: %v", err)
@@ -43,10 +55,11 @@ func main() {
 		log.Printf("Temperature: %s, Pressure: %s", temp, pres)
 
 		scr.Box(1, 5, 120, 55, false) // Clear area
-		scr.DrawText(10, 10, "Temp: "+temp)
-		scr.DrawText(10, 30, "Pres: "+pres)
+		scr.DrawTextCentered(64+shiftX, 15+shiftY, "Temp: "+temp)
+		scr.DrawTextCentered(64+shiftX, 35+shiftY, "Pres: "+pres)
 
 		disp.Update()
+		i++
 	}
 
 	log.Println("OLED initialized, test pixels drawn.")
